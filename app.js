af084807890b7d9cdbd95dc8c09c7c1982139ab9
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-20 23:43:43 -0700

    Add first step to adding Header component

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-// Learning React (with JSX: A syntax extension to JS that is used with React to describe elements in the UI)
+// Learning React (with JSX and components)
 
-// Step 1:
-// Since JS acts as an extension to the JS language, it accepts any valid JS expressions written inside {}, this is called a JSX expression. They can be placed between JSX tags or as the value of an attribute in the JSX tag.
-const desc =
-  "I just learned how to create a React node and render it into the DOM.";
-const myTitleID = "main-title";
-const name = "Tiffany";
+// Components
+// A component is a piece of UI that you can reuse
+// Creating a React Component is a two step process:
+// Step 1: Define the component as either a JS function or class
+// Setp 2: Display the component in the UI with a JSX tag
 
-// Step 2:
-// This isn't really HTML or JS syntax. It's JSX:
-// JSX can contain children or nested elements
-const header = (
-  <header>
-    <h1 id={myTitleID}>{name}'s First React Element (with Guil)!</h1>
-    {/* React uses a camel case property naming convention instead of the usual HTML attribute names. Example:
-    Use className instead of class (because class is a reserved word in JS used to define classes) */}
-    <p className="main-desc">{desc}</p>
-    <input value={10 * 20} />
-    {/* Wherever you're including the curly-braces, you're exiting JSX and entering JS territory: */}
-  </header>
-);
-// Step 2 end
+// React components are required to begin with an upper case letter
+// Header is going to return react elements describing what should appear on the screen using JSX
+function Header() {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+}
 
 ReactDOM.render(header, document.getElementById("root"));
-// Step 1 end
