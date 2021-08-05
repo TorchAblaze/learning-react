ce3f09e5158274633ec5e0953d70192446520d89
TorchAblaze <tiffanygreathead@gmail.com>
2021-08-05 13:51:09 -0700

    Add state notes

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// In most apps, you'll have a list of sibling elements that get updated, removed, added and reordered on the page.
-// Key - a unique identifier that gives React a way to quickly and reliably identify an element in the list
-// React does not recommend using index for unique keys, because the index might not always uniquely identify elements. It's usually best to use a unique id.
+// There are two ways that data gets handled in react: props and state.
+// State is used to store information about a component that can change over time.
 
-// Step 1: Add an id property for each player
 const players = [
   {
     name: "Tiffany",
@@ -25,7 +23,6 @@ const players = [
     id: 4,
   },
 ];
-// Step 1 end
 
 function Header(props) {
   console.log(props);
@@ -56,8 +53,6 @@ const Counter = (props) => {
   );
 };
 
-// Step 2: Pass Player a new Key prop
-// The React docs recommend using a string as the key value, so use the toString() method to convert the id value to a string
 const App = (props) => {
   return (
     <div className="scoreboard">
@@ -72,13 +67,8 @@ const App = (props) => {
     </div>
   );
 };
-// Step 2 end
 
 ReactDOM.render(
   <App initialPlayers={players} />,
   document.getElementById("root")
 );
-
-// Not all React elements need a key prop
-// Pass a key prop anytime you're creating elements by iterating over an array of items that will be rearranged, added or deleted in your UI
-// The key will help React identify which items were changed, added or removed from the DOM.
